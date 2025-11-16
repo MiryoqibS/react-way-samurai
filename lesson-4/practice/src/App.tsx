c8f61248156707aa847ec1bcc700a7202128fecf
Miryoqib <miryoqibcoder@gmail.com>
2025-11-16 20:27:06 +0500

    Тема 4 - React-приложение с нуля

---
+++ lesson-4/practice/src/App.tsx
@@ -0,0 +1,44 @@
+
+function App() {
+  const tracks = [
+    {
+      id: 0,
+      title: "Musicfun soundtrack",
+      url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3"
+    },
+    {
+      id: 1,
+      title: "Musicfun soundtrack instrumental",
+      url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3"
+    },
+    {
+      id: 2,
+      title: "Musicfun soundtrack instrumental",
+      url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3"
+    },
+    {
+      id: 3,
+      title: "Musicfun soundtrack instrumental",
+      url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3"
+    },
+  ];
+
+  return (
+    <div>
+      <h1>Musicfun Player</h1>
+      <ul>
+        {tracks.map(track => (
+          <li key={track.id}>
+            <div>
+              {track.title}
+            </div>
+            <audio src={track.url}
+              controls></audio>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App;
